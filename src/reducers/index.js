b787002eb7259822a441e8a3273b7bd067f7e28c
masa <iwamasa.0526.to@gmail.com>
2020-11-22 09:02:10 +0900

    refactor events reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,5 @@
-import { CREATE_EVENT, DELETE_EVENT, DELETE_ALL_EVENT } from "../actions";
+import { combineReducers } from "redux";
 
-const events = (state = [], action) => {
-  switch (action.type) {
-    case CREATE_EVENT:
-      const event = { title: action.title, body: action.body };
-      const length = state.length;
-      const id = length === 0 ? 1 : state[length - 1].id + 1; /*三項演算子 */
-      return [...state, { id, ...event }];
-    case DELETE_EVENT:
-      return state.filter((event) => event.id !== action.id);
-    case DELETE_ALL_EVENT:
-      return [];
-    default:
-      return state;
-  }
-};
+import events from "./events";
 
-export default events;
-
-//reducerは二つの引数を受け取る。
-//stateは未定義の場合があるから、最初から初期化しておく事もできる。
-//actionには常に「type」という属性が渡ってくる。
-//id = state[ length - 1] →　最後の要素を取り出せる。
+export default combineReducers({ events });
